613de901f4f1578a32eb28091ddab1471818e319
Kashif Chaudhry <kchaudhry@edx.org>
2017-12-12 16:51:26 +0500

    new page wait

---
+++ regression/tests/helpers/new_page_object.py
@@ -0,0 +1,61 @@
+from bok_choy.page_object import PageObject, unguarded
+from bok_choy.promise import Promise, EmptyPromise, BrokenPromise
+
+
+
+class NewPageObject(PageObject):
+
+
+    @unguarded
+    def wait_for_page(self, timeout=30):
+        """
+        Block until the page loads, then returns the page.
+        Useful for ensuring that we navigate successfully to a particular page.
+
+        Keyword Args:
+            timeout (int): The number of seconds to wait for the page before timing out with an exception.
+
+        Raises:
+            BrokenPromise: The timeout is exceeded without the page loading successfully.
+        """
+
+        def _is_document_ready_interactive():
+            """
+            Check the loading state of the document to ensure the document and all sub-resources
+            have finished loading (the document load event has been fired.)
+            """
+            return self.browser.execute_script(
+                "return document.readyState=='interactive'")
+
+
+        def _is_document_ready():
+            """
+            Check the loading state of the document to ensure the document and all sub-resources
+            have finished loading (the document load event has been fired.)
+            """
+            return self.browser.execute_script(
+                "return document.readyState=='complete'")
+
+        try:
+            EmptyPromise(
+                _is_document_ready,
+                "The document and all sub-resources have finished loading.",
+                timeout=timeout
+            ).fulfill()
+        except BrokenPromise:
+
+            EmptyPromise(
+                _is_document_ready_interactive,
+                "The document and all sub-resources have finished loading.",
+                timeout=timeout
+            ).fulfill()
+
+        result = Promise(
+            lambda: (self.is_browser_on_page(), self), "loaded page {!r}".format(self),
+            timeout=timeout
+        ).fulfill()
+
+        if self.verify_accessibility:
+            self.a11y_audit.check_for_accessibility_errors()  # pylint: disable=no-member
+
+        return result
